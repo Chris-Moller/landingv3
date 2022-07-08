57fe43543d14abf51a8c58375f1d126650bf7c28
artixadmin <vkrishnan@layrlabs.org>
2022-07-07 17:14:49 -0700

    Ethconomics Vid
    Video Update

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -163,11 +163,9 @@ export default function Home() {
                 <Paper
                   className="my-card"
                   sx={{
-                    cursor: "pointer",
-                    "&:hover": {
+                    cursor: "pointer",                    
                       background:
-                        "linear-gradient(180deg, rgba(18, 25, 39, 0) 0%, rgba(18, 25, 39, 0.25) 54.69%, rgba(206, 137, 33, 0.5) 100%)",
-                    },
+                        "linear-gradient(180deg, rgba(18, 25, 39, 0) 0%, rgba(18, 25, 39, 0.25) 54.69%, rgba(206, 137, 33, 0.5) 100%)",                  
                   }}
                 >
                   <Typography
@@ -187,10 +185,9 @@ export default function Home() {
                   className="my-card"
                   sx={{
                     cursor: "pointer",
-                    "&:hover": {
+                    
                       background:
-                        "linear-gradient(180deg, rgba(18, 25, 39, 0) 0%, rgba(18, 25, 39, 0.25) 54.69%, rgba(44, 138, 98, 0.5) 100%)",
-                    },
+                        "linear-gradient(180deg, rgba(18, 25, 39, 0) 0%, rgba(18, 25, 39, 0.25) 54.69%, rgba(44, 138, 98, 0.5) 100%)",                    
                   }}
                 >
                   <Typography
--- src/views/solutions/index.js
+++ src/views/solutions/index.js
@@ -5,7 +5,9 @@ export default function Solutions() {
     <MainContainer sx={{ padding: "100px 0" }}>
       <div>
         <h1>Resources:</h1>
-        <p>Coming Soon</p>
+        <p align="center">
+        <iframe width="800" height="500" src="https://www.youtube.com/embed/01xDSwMO5U4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </p>
       </div>
     </MainContainer>
   );
