977f2460427949345a56d6f158c19a25b5eef10d
artixadmin <vkrishnan@layrlabs.org>
2022-07-07 17:28:40 -0700

    Fix Learn More
    Fix Button

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,10 +1,38 @@
+import { Link as RouterLink, useNavigate } from "react-router-dom";
+import { styled, useTheme } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
-import { useTheme } from "@mui/material/styles";
 import BlueButton from "components/shared-components/BlueButton";
 import MainContainer from "components/shared-components/MainContainer";
+
+const Link = styled(RouterLink)(() => ({
+  textDecoration: "none",
+  fontSize: "16px",
+  fontWeight: 600,
+  color: "#D8F8FF",
+  position: "relative",
+  "&:after": {
+    content: "''",
+    display: "block",
+    position: "absolute",
+    bottom: -12,
+    left: 0,
+    width: 0,
+    height: 3,
+    borderRadius: "10px",
+    backgroundColor: "#1BA8C7",
+    transition: "all .2s ease-out",
+  },
+  "&:hover": {
+    fontWeight: 700,
+    color: "#ECFCFF",
+    "&:after": {
+      width: 25,
+    },
+  },
+}));
 
 export default function Home() {
   const theme = useTheme();
@@ -53,7 +81,7 @@ export default function Home() {
                 </Typography>
               </Box>
               <Box>
-                <BlueButton sx={{ width: 154, position: "relative", zIndex: 2 }} onClick={event =>  window.location.href='/solutions'}>Learn More</BlueButton>
+              <Link to="/solutions"><BlueButton sx={{ width: 154, position: "relative", zIndex: 2 }}>Learn More</BlueButton></Link>                
               </Box>
             </Grid>
             <Grid item md={6} xs={12}>
