7638e1b07cebbccff43ae625088e387ca96e5339
artixadmin <vkrishnan@layrlabs.org>
2022-07-07 17:31:29 -0700

    fix button
    fixed learn more

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,4 +1,4 @@
-import { Link as RouterLink, useNavigate } from "react-router-dom";
+import { Link as RouterLink} from "react-router-dom";
 import { styled, useTheme } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
