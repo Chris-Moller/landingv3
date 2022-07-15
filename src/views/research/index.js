6e7f0e9b9ca087c6feb349dcaa10d9545c56e9a7
artixadmin <vkrishnan@layrlabs.org>
2022-07-15 09:46:33 -0700

    Comment Link
    Might need in future.

--- src/views/research/index.js
+++ src/views/research/index.js
@@ -7,32 +7,32 @@ import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 import BlueButton from "components/shared-components/BlueButton";
 
-const Link = styled(RouterLink)(() => ({
-  textDecoration: "none",
-  fontSize: "16px",
-  fontWeight: 600,
-  color: "#D8F8FF",
-  position: "relative",
-  "&:after": {
-    content: "''",
-    display: "block",
-    position: "absolute",
-    bottom: -12,
-    left: 0,
-    width: 0,
-    height: 3,
-    borderRadius: "10px",
-    backgroundColor: "#1BA8C7",
-    transition: "all .2s ease-out",
-  },
-  "&:hover": {
-    fontWeight: 700,
-    color: "#ECFCFF",
-    "&:after": {
-      width: 25,
-    },
-  },
-}));
+// const Link = styled(RouterLink)(() => ({
+//   textDecoration: "none",
+//   fontSize: "16px",
+//   fontWeight: 600,
+//   color: "#D8F8FF",
+//   position: "relative",
+//   "&:after": {
+//     content: "''",
+//     display: "block",
+//     position: "absolute",
+//     bottom: -12,
+//     left: 0,
+//     width: 0,
+//     height: 3,
+//     borderRadius: "10px",
+//     backgroundColor: "#1BA8C7",
+//     transition: "all .2s ease-out",
+//   },
+//   "&:hover": {
+//     fontWeight: 700,
+//     color: "#ECFCFF",
+//     "&:after": {
+//       width: 25,
+//     },
+//   },
+// }));
 
 
 export default function Solutions() {
