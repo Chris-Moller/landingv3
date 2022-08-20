66be2aec353856d4db342410e53c2ae5b8109cdb
Chris Moller <cmoller02@gmail.com>
2022-08-20 19:03:13 -0400

    push-3

--- src/components/shared-components/MainContainer.js
+++ src/components/shared-components/MainContainer.js
@@ -6,7 +6,7 @@ const MainContainer = styled(Box)(({ theme }) => ({
   margin: "auto",
   [theme.breakpoints.down("xl")]: { maxWidth: 1000 },
   [theme.breakpoints.down("lg")]: { maxWidth: 750 },
-  [theme.breakpoints.down("md")]: { maxWidth: 500 },
+  [theme.breakpoints.down("md")]: { maxWidth: 600 },
   [theme.breakpoints.down("sm")]: {
     maxWidth: "450px",
     padding: "0 20px",
